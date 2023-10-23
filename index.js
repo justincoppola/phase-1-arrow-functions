6a823965fa08823b7f7d37f561a3221a81326b0a
justin <justincoppola6@gmail.com>
2023-10-23 14:25:05 -0400

    God is good

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Arrow function
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+  
